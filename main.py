7b6ca9d2c315bcb8d9bf3dab6c53e8bf1d323170
grebacz <gabriel.rebacz@enigma.com.pl>
2019-12-29 23:49:57 +0100

    point generation and writing to file

--- main.py
+++ main.py
@@ -5,42 +5,55 @@ import math
 from datafile import *
 
 N = sum(czyN)
+file = open("results.txt", "w+")
+fix_function = False
+
+
+# todo
+def fix_point(point):
+    pass
+    return 0
 
 
 class Solution:
-    def __init__(self, point, fix=False):
+    def __init__(self, point):
         self.point = point
-        self.value = cost_objective_function(point)
-
-
-def fix_objective_function(solution_matrix):
-    # todo
-    print('todo')
+        if fix_function:
+            self.fixed_point = fix_point(point)
+            self.value = value_function(self.fixed_point)
+        else:
+            self.value = value_function(point)
 
 
 def greedy_pick_point_gen():
     point = np.zeros((A, P), dtype=int)
-    for i in range(0, A - 1):
-        param_list = [(j, w[i][j] / u[i][j]) for j in range(0, P)]
+    for i in range(A):
+        param_list = [((j, w[i][j] / u[i][j]) if u[i][j] != 0 else (j, 0)) for j in range(0, P)]
         param_list.sort(key=lambda x: x[1], reverse=True)
         u_value = u[i][param_list[0][0]]
         k = 1
-        while u_value < 4 * udzial[i]:
+        while u_value <= 4 * udzial[i]:
             point[i][param_list[k - 1][0]] = 1
+            if k >= P-1:
+                break
             u_value += u[i][param_list[k][0]]
+            k += 1
 
     return point
 
 
 def random_pick_point_gen():
     point = np.zeros((A, P), dtype=int)
-    for i in range(0, A - 1):
-        indexes = random.sample(range(0, P), P)
+    for i in range(A):
+        indexes = random.sample(range(P), P)
         u_value = u[i][indexes[0]]
         k = 1
-        while u_value < 4 * udzial[i]:
+        while u_value <= 4 * udzial[i]:
             point[i][indexes[k - 1]] = 1
             u_value += u[i][indexes[k]]
+            if k >= P-1:
+                break
+            k += 1
 
     return point
 
@@ -49,11 +62,11 @@ def gen_starting_points():
     points = [Solution(np.zeros((A, P), dtype=int)), Solution(np.ones((A, P), dtype=int)),
               Solution(greedy_pick_point_gen())]
     for i in range(25):
-        points.append(random_pick_point_gen())
+        points.append(Solution(random_pick_point_gen()))
     return points
 
 
-def cost_objective_function(solution_matrix, k=250):
+def value_function(solution_matrix, k=250):
     value = 0
     cost2 = 0
     for i in range(A):
@@ -77,8 +90,8 @@ def cost_objective_function(solution_matrix, k=250):
 def randomly_change_n_positions(chosen_publications, n):
     new_point = copy.deepcopy(chosen_publications.point)
     for k in range(n):
-        i = random.randint(A)
-        j = random.randint(P)
+        i = random.randint(0, A-1)
+        j = random.randint(0, P-1)
         if new_point[i][j] > 0:
             new_point[i][j] = 0
         else:
@@ -87,18 +100,17 @@ def randomly_change_n_positions(chosen_publications, n):
     return Solution(new_point)
 
 
-def variable_neighborhood_search(init_solution=random_pick_point_gen(), search_proportion=1 / 10,
+def variable_neighborhood_search(init_solution, search_proportion=1 / 10,
                                  max_neighborhood_radius=A * P):
+    best_solution = init_solution
     count = 0
-    best_solution = copy.deepcopy(init_solution)
-    n = math.floor(search_proportion * A * P)
-    radius = 1
     iteration = 0
+    radius = 1
     stage = 10
     stop = False
     while not stop:
         neighborhood = []
-        for i in range(n * radius):
+        for i in range(math.floor(search_proportion * A * P * radius)):
             neighborhood.append(randomly_change_n_positions(best_solution, radius))
             count += 1
             if count >= stage * A * P:
@@ -110,7 +122,7 @@ def variable_neighborhood_search(init_solution=random_pick_point_gen(), search_p
 
         best_in_neighborhood = max(neighborhood, key=lambda x: x.value)
         if best_in_neighborhood.value > best_solution.value:
-            best_solution = copy.deepcopy(best_in_neighborhood)
+            best_solution = best_in_neighborhood
             radius = 1
         else:
             radius += 1
@@ -125,13 +137,29 @@ def variable_neighborhood_search(init_solution=random_pick_point_gen(), search_p
 # Run script
 def main():
     print("Algorithm start")
+    print("Cost function")
+    file.write("---Cost function---")
     points = gen_starting_points()
     for i, point in enumerate(points):
-        print("Calculating point ", i)
+        print("Calculating point " + str(i))
+        file.write("Calculating point " + str(i))
         variable_neighborhood_search(point)
+
+    # print("Fix function")
+    # file.write("---Fix function---")
+    # global fix_function
+    # fix_function = True
+    # for i, point in enumerate(points):
+    #     file.write("Calculating point " + str(i))
+    #     variable_neighborhood_search(point)
+
     print("Algorithm end")
 
 
-if __name__ == '__main__':
-    file = open("results", "w+")
+if __name__ == "__main__":
     main()
+
+
+
+
+
