b6bc3d21ed096a635db0b5c7de52a57b0af5e4e7
grebacz <gabriel.rebacz@enigma.com.pl>
2019-12-29 15:18:19 +0100

    new algorithm, changed solution array to class

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import numpy as np
 import random
 import copy
+import math
 from datafile import *
+
+class Solution:
+    def __init__(self, point):
+        self.point = point
+        self.value = cost_objective_function(point)
 
 def fix_objective_function(solution_matrix):
     print('todo')
 
+
+def gen_starting_points():
+    points = []
+    points.append(Solution(np.zeros((A, P), dtype=int)))
+    points.append(Solution(np.ones((A, P), dtype=int)))
 
 def cost_objective_function(solution_matrix, k=250):
     value = 0
@@ -17,71 +28,60 @@ def cost_objective_function(solution_matrix, k=250):
             cost1 += solution_matrix[i][j] * u[i][j]
             cost2 += solution_matrix[i][j] * u[i][j]
         cost1 -= 4 * udzial[i]
+        cost2 -= 3*udzial[i]
         if cost1 > 0:
-            value -= k * cost1
+            value -= k * (1 + cost1)
 
     if cost2 > 0:
-        value -= k * cost2
+        value -= k * (1 + cost2)
 
     return value
 
 
 def seed_solution():
     matrix = np.random.randint(2, size=(A, P))
-    value = cost_objective_function(matrix)
-    return [matrix, value]
+    return Solution(matrix)
 
 
 def randomly_change_n_positions(chosen_publications, n):
-    new_solution = copy.deepcopy(chosen_publications)
+    new_point = copy.deepcopy(chosen_publications.point)
     for k in range(n):
         i = random.randint(0, A-1)
         j = random.randint(0, P - 1)
-        if new_solution[0][i][j] > 0:
-            new_solution[0][i][j] = 0
+        if new_point[i][j] > 0:
+            new_point[i][j] = 0
         else:
-            new_solution[0][i][j] = 1
+            new_point[i][j] = 1
 
-    new_solution[1] = cost_objective_function(new_solution[0])
-    return new_solution
+    return Solution(new_point)
 
 
-def local_search(chosen_publications, max_attempts=50, neighbourhood_size=5):
+def variable_neighborhood_search(init_solution=seed_solution(), search_proportion = 1/10, max_neighborhood_radius=50):
     count = 0
-    new_solution = copy.deepcopy(chosen_publications)
-    k = 1
-    while count < max_attempts:
-        candidate = randomly_change_n_positions(new_solution, k)
-        if candidate[1] > new_solution[1]:
-            new_solution = copy.deepcopy(candidate)
-            k = 1
-            count = 0
+    best_solution = copy.deepcopy(init_solution)
+    #max number of solutions to find in neighborhood 1 - to improve?
+    n = math.floor(search_proportion * A * P)
+    radius = 1
+    iteration = 0
+    while count < 1000 * A * P:
+        neighborhood = []
+        for i in range(n*radius):
+            neighborhood.append(randomly_change_n_positions(best_solution, radius))
+            count += 1
+        best_in_neighborhood = max(neighborhood, key=lambda x: x.value)
+        if best_in_neighborhood.value > best_solution.value:
+            best_solution = copy.deepcopy(best_in_neighborhood)
+            radius = 1
         else:
-            k += 1
-            if k > neighbourhood_size:
-                k = 1
-            count = count + 1
-    return new_solution
+            radius += 1
+            if radius > max_neighborhood_radius:
+                radius = 1
+        print("Iteration = ", iteration, ">> Score ", best_solution.value, "count = ", count)
+        iteration += 1
 
-
-def variable_neighborhood_search(max_attempts=20, neighbourhood_size=100, iterations=1000):
-    count = 0
-    new_solution = seed_solution()
-    best_solution = copy.deepcopy(new_solution)
-    while count < iterations:
-        for i in range(1, neighbourhood_size+1):
-            new_solution = randomly_change_n_positions(best_solution, i)
-            new_solution = local_search(chosen_publications=new_solution, max_attempts=max_attempts,
-                                        neighbourhood_size=neighbourhood_size)
-            if new_solution[1] > best_solution[1]:
-                best_solution = copy.deepcopy(new_solution)
-                break
-        count = count + 1
-        print("Iteration = ", count, ">> Score ", best_solution[1])
     return best_solution
-
 
 ################# Run script #####################
 print("Algorithm start")
-solution = variable_neighborhood_search()
+solution = variable_neighborhood_search(seed_solution())
 print('End - best solution: ' + str(solution))
